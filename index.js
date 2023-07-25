efb32b825eec40a557ae5ef2d7c6e3056bf8caf2
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-07-25 09:21:10 +0600

    jwt expires date increase to 15days

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function run() {
         app.post('/login', async (req, res) => {
             const user = req.body;
             const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1d',
+                expiresIn: '15d',
             });
             res.send({ accessToken });
         });
